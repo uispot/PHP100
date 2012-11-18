92fd5e7d0a7331510f3077e85e84593f00e5c463
a-jian <uispot@qq.com>
2012-11-18 09:29:29 +0800

    99乘法表

---
+++ demo/9X9.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+	<title></title>
+</head>
+<body>
+<?php
+
+//99乘法表
+//从1到9
+	for($i=1; $i<=9; $i++)
+	{
+		for($j=1; $j<=9; $j++)
+		{
+			if($j <=$i){
+				echo $j . "*" . $i . "=" .($i*$j) . " | " ;
+			};
+		}
+		echo "<br>";
+	}
+
+	echo "<hr>";
+
+	for($i=9; $i>=1; $i--)
+	{
+		for($j=1; $j<=9; $j++)
+		{
+			if($j <=$i){
+				echo $i . "*" . $j . "=" .($i*$j) . " | " ;
+			};
+		}
+		echo "<br>";
+	}
+
+
+?>
+</body>
+</html>
